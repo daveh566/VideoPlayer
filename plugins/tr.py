79bd361b396a39b6d3592a43d2e500fc4facd0bb
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-09-14 11:16:17 +0300

    Update tr.py

--- plugins/tr.py
+++ plugins/tr.py
@@ -2,6 +2,6 @@ import os
 from config import ASSISTANT_NAME
 
 HELP_TEXT =f"**SETTINGS** \n\n`1) summon me , don't forget to Make me an Admin` \n2) **Add** @{ASSISTANT_NAME} **to your Group** \n\n**COMMANDS** \n\n- `/stream` **: Reply to Video or File That You Want To stream In Vc.** \n- `/stopstream`  **: Stop the stream** \n- `cplay` **: Stream in channels, make sure I'm an Admin in your channel**"
-START_TEXT = '👋 `HEYA I'M` **NellyXVcPlayer** \n\n` Enjoy your streaming`'
+START_TEXT = '👋 `HEYA My Name Is` **NellyXVcPlayer** \n\n`Lets Enjoy with your friends`'
 ABOUT_TEXT = '**INFORMATION** \n\n`🤖  I stream videos in telegram group video chats using several methods from WebRTC.` \n\n`💡 Powered by PyTgcalls the Async client API for the Telegram Group Calls, and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots.` \n\n**I'm licensed under GNU-GPL 3.0 License**'
 DEVS_TEXT =f"**CREDITS** \n\n`These are my masters  Helping in Making The` @{ASSISTANT_NAME} `Bot.`"
