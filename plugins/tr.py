a23d34227d424e20374afefa5ee3f3b4d616ed1c
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-09-14 08:44:39 +0300

    Update tr.py

--- plugins/tr.py
+++ plugins/tr.py
@@ -1,7 +1,7 @@
 import os
 from config import ASSISTANT_NAME
 
-HELP_TEXT =f"**SETTINGS** \n\n`1) Add this Bot to your Group and Make it Admin` \n2) **Add** @{ASSISTANT_NAME} **to your Group** \n\n**COMMANDS** \n\n- `/stream` **: Reply to Video or File That You Want To stream In Vc.** \n- `/stopstream`  **: Stop the stream**"
-START_TEXT = '👋 `Hey My Name Is` **VideoPlayer** \n\n`Lets Enjoy Cinematic View  of Group Video Player With Your Friends ❤️😊`'
-ABOUT_TEXT = '**👩‍💻 INFORMATION** \n\n`🤖 This bot streams videos in telegram group video chats using several methods from WebRTC.` \n\n`💡 Powered by PyTgcalls the Async client API for the Telegram Group Calls, and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots.` \n\n**This bot licensed under GNU-GPL 3.0 License**'
-DEVS_TEXT =f"**CREDIT VIDEO PLAYER** \n\n`Here Some Developers Helping in Making The` @{ASSISTANT_NAME} `Bot.`"
+HELP_TEXT =f"**SETTINGS** \n\n`1) summon me , don't forget to Make me an Admin` \n2) **Add** @{ASSISTANT_NAME} **to your Group** \n\n**COMMANDS** \n\n- `/stream` **: Reply to Video or File That You Want To stream In Vc.** \n- `/stopstream`  **: Stop the stream**"
+START_TEXT = '👋 `HEYA I'M` **NellyXVcPlayer** \n\n` Enjoy your streaming [Owner](https://t.me/aspirer2)`'
+ABOUT_TEXT = '**INFORMATION** \n\n`🤖  I stream videos in telegram group video chats using several methods from WebRTC.` \n\n`💡 Powered by PyTgcalls the Async client API for the Telegram Group Calls, and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots.` \n\n**I'm licensed under GNU-GPL 3.0 License**'
+DEVS_TEXT =f"**CREDITS** \n\n`These are my masters  Helping in Making The` @{ASSISTANT_NAME} `Bot.`"
